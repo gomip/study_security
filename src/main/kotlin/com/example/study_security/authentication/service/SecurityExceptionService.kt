d374d8afd859e6a441c6b51e723232f7d1032a45
gomip <rlawjdgns15@hotmail.com>
2023-11-23 17:20:58 +0900

    오류 출력 로직 추가

--- src/main/kotlin/com/example/study_security/authentication/service/SecurityExceptionService.kt
+++ src/main/kotlin/com/example/study_security/authentication/service/SecurityExceptionService.kt
@@ -2,10 +2,15 @@ package com.example.study_security.authentication.service
 
 import com.example.study_security.authentication.dto.ExceptionResponseDto
 import com.example.study_security.authentication.enumerable.ErrorCode
+import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.http.HttpStatus
 
 class SecurityExceptionService {
+
     companion object {
+        private val objectMapper = ObjectMapper()
+
         fun onUnknownException(exception: Exception, response: HttpServletResponse) {
             val exceptionResponse = ExceptionResponseDto.of(
                 ErrorCode.UNKNOWN_SERVER_ERROR,
@@ -13,17 +18,19 @@ class SecurityExceptionService {
             )
 
             exception.printStackTrace()
-            prin
+            printError(HttpStatus.INTERNAL_SERVER_ERROR, response, exceptionResponse)
+        }
+
+        private fun printError(
+            httpStatus: HttpStatus,
+            response: HttpServletResponse,
+            exceptionResponseDto: ExceptionResponseDto,
+        ) {
+            response.contentType = "application/json"
+            response.characterEncoding = "UTF-8"
+            response.status = httpStatus.value()
+
+            objectMapper.writeValue(response.writer, exceptionResponseDto)
         }
     }
 }
-/*
-*         fun onException(exception: Exception, response: HttpServletResponse) {
-            val exceptionResponseDto = ExceptionResponseDto.of(
-                ErrorCode.UNKNOWN_SERVER_ERROR,
-                exception.message ?: "알 수 없는 에러",
-            )
-
-            exception.printStackTrace()
-            printError(HttpStatus.INTERNAL_SERVER_ERROR, response, exceptionResponseDto)
-        }*/
